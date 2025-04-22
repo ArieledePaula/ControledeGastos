ebe89d8f963b8694164252f05ffa9ab5f35fb127
Luna Vulperya <lunavulperya@gmail.com>
2025-04-22 20:48:42 -0300

    Mudando toda a estrutura do código para Adicionar Categoria e estilizando cabeçalho

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+var dadosCategoria = [];
+
+function adicionarCategoria(){
+    let Categoria = document.getElementById('Categoria').value;
+
+    if(Categoria){
+        dadosCategoria.push(Categoria);
+        console.log(dadosCategoria);
+        criarCategoria();
+        document.getElementById('Categoria').value = "";
+    }else{
+        alert("Nenhuma categoria foi adicionada")
+    }
+}
+
+
+function criarCategoria(){
+    let tabela = "<tr><td>Nome</td><td>Ações</td></tr>";
+    for(let i =0;i<=(dadosCategoria.length-1); i++){
+        tabela += "<tr><td>" + dadosCategoria[i] + "</td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
+
 var dadosItens= [];
 
 function adicionarItem(){
@@ -12,6 +36,7 @@ function adicionarItem(){
         alert("Nenhum item foi adicionado!")
     }
 }
+
 
 function criarLista(){
     for(let i = 0;i<=(dadosItens.length-1);i++){
