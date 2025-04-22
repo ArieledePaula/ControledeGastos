d64fff27812612dcce59f66a9db88d6bbf12b835
Luna Vulperya <lunavulperya@gmail.com>
2025-04-22 19:55:51 -0300

    Criando Lista para adicionar Itens no JS para o tópico Alimentação

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function adiItem(){
     let adiItem =document.getElementById('Item').value;
 }
 
+function criarLista(){
+    let tabela = "<tr><th></th></tr>";
+    for(let i = 0;i<=(dadosItens.length-1);i++){
+        tabela =+ "<tr><th>Alimentação</th></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
