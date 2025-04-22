317cce3b92a50b3b2c86654b1fb99dcb90fb6ba2
Luna Vulperya <lunavulperya@gmail.com>
2025-04-22 20:12:39 -0300

    Fazendo a função Adicionar Item ser funcional

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 var dadosItens= [];
 
-function adiItem(){
-    let adiItem =document.getElementById('Item').value;
+function adicionarItem(){
+    let Item =document.getElementById('Item').value;
+
+    if(Item){
+        dadosItens.push(Item);
+        console.log(dadosItens);
+        criarLista();
+        document.getElementById('Item').value = "";
+    }else{
+        alert("Nenhum item foi adicionado!")
+    }
 }
 
 function criarLista(){
-    let tabela = "<tr><th></th></tr>";
+    let tabela = "<tr><td></td></tr>";
     for(let i = 0;i<=(dadosItens.length-1);i++){
-        tabela =+ "<tr><th>Alimentação</th></tr>";
+        tabela =+ "<tr><td>" + dadosItens[i] + "</td></tr>"
         document.getElementById('tabela').innerHTML = tabela;
     }
 }
