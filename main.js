fddbe13272bd1e0c33370395fd62965d255cfba2
Luna Vulperya <lunavulperya@gmail.com>
2025-04-22 22:12:53 -0300

    Ajustando "Categoria" e "Item"  e adicionando preço no JS

--- main.js
+++ main.js
@@ -5,45 +5,35 @@ function adicionarCategoria(){
 
     if(Categoria){
         dadosCategoria.push(Categoria);
-        console.log(dadosCategoria);
         criarCategoria();
         document.getElementById('Categoria').value = "";
     }else{
-        alert("Nenhuma categoria foi adicionada")
+        alert("Nenhuma categoria foi adicionada");
     }
 }
 
-
 function criarCategoria(){
-    let tabela = "<tr><td></td></tr>";
-    for(let i =0;i<=(dadosCategoria.length-1); i++){
-        tabela += "<tr><td>" + dadosCategoria[i] +  "</td></tr>";
-        document.getElementById('tabela').innerHTML = tabela;
+    let tabela = "";
+    for(let i = 0; i < dadosCategoria.length; i++){
+        tabela += "<tr><td>" + dadosCategoria[i] + "</td></tr>";
     }
+    document.getElementById('tabela').getElementsByTagName('tbody')[0].innerHTML = tabela;
 }
 
 var dadosItem = [];
 
 function adicionarItem(){
     let Item = document.getElementById('Item').value;
+    let Preco = parseFloat(document.getElementById('Preco').value);
 
-    if(Item){
-        dadosItem.push(Item);
-        console.log(dadosItem);
+    if(Item && !isNaN(Preco)){
+        dadosItem.push({ nome: Item, valor: Preco });
         criarItem();
+        atualizarTotal(); 
         document.getElementById('Item').value = "";
+        document.getElementById('Preco').value = "";
     }else{
-        alert("Nenhum Item foi adicionado!")
+        alert("Adicione o nome do item e o preço!");
     }
 }
-
-
-function criarItem(){
-    let tabela1 = "<tr><td></td></tr>";
-    for(let i =0;i<=(dadosItem.length-1); i++){
-        tabela1 += "<tr><td>" + dadosItem[i] +  "</td></tr>";
-        document.getElementById('tabela1').innerHTML = tabela1;
-    }
-}
-
 
